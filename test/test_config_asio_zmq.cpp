bada683ab20f5cba1a40782170b1a7ca6eb4c68c
Oleksandr Slovak <slovak194@gmail.com>
2021-01-03 21:16:47 +0100

    refactor

--- test/test_config_asio_zmq.cpp
+++ test/test_config_asio_zmq.cpp
@@ -14,25 +14,55 @@ using namespace std::chrono_literals;
 
 namespace asio = boost::asio;
 
-class ZmqHandler : public asio::io_service {
-  std::shared_ptr<nlohmann::json> m_storage;
-  std::mutex m_storage_mtx;
- public:
-  azmq::rep_socket m_responder;
-  std::vector<std::uint8_t> m_buf;
+class RemoteConfigSever : private asio::io_service {
 
-  explicit ZmqHandler(const std::string &config_path)
+ public:
+
+  std::string m_config_path;
+  std::string m_tmp_json_file_path;
+
+  explicit RemoteConfigSever(const std::string &config_path, const std::string &addr = "tcp://127.0.0.1:5555")
       : m_responder(*this) {
-    m_responder.bind("tcp://127.0.0.1:5555");
+    m_responder.bind(addr);
 
     m_buf.reserve(256);
 
-    const std::string tmp_json_file_path = config_path + ".json";
+    m_storage = std::make_shared<nlohmann::json>();
+
+    LoadConfig(config_path);
+
+    ScheduleReceive();
+
+    std::thread thread([this]() { this->run(); });
+
+    thread.detach();
+
+  }
+
+  nlohmann::json operator()(const std::string &key) {
+    return Get(key);
+  }
+
+ private:
+
+  std::shared_ptr<nlohmann::json> m_storage;
+  std::mutex m_storage_mtx;
+
+  azmq::rep_socket m_responder;
+  std::vector<std::uint8_t> m_buf;
+
+  void LoadConfig(const std::string &config_path = "") {
+
+    if (!config_path.empty()) {
+      this->m_config_path = config_path;
+      this->m_tmp_json_file_path = config_path + ".json";
+    }
+
     std::string command = std::string("python3 -c '") +
-        std::string("config_file_yaml_path = \"") + config_path + std::string("\"\n") +
+        std::string("config_file_yaml_path = \"") + this->m_config_path + std::string("\"\n") +
         "import yaml\n"
         "import json\n"
-        "with open(config_file_yaml_path, \"r\") as yaml_in, open(\"" + tmp_json_file_path +
+        "with open(config_file_yaml_path, \"r\") as yaml_in, open(\"" + this->m_tmp_json_file_path +
         "\", \"w\") as json_out:\n"
         "    yaml_object = yaml.safe_load(yaml_in)\n"
         "    json.dump(yaml_object, json_out)\n"
@@ -46,25 +76,18 @@ class ZmqHandler : public asio::io_service {
       throw std::runtime_error("Conversion yaml to json has failed with exit code: " + std::to_string(res));
     }
 
-    std::ifstream i(tmp_json_file_path);
-    m_storage = std::make_shared<nlohmann::json>();
-    *m_storage = nlohmann::json::parse(i);
+    std::ifstream i(this->m_tmp_json_file_path);
 
-    std::ofstream o(tmp_json_file_path);
+    this->Set(nlohmann::json::parse(i));
 
-    o << (*m_storage).dump(1, '\t');
-
-    ScheduleReceive();
-
-    std::thread thread([this]() { this->run(); });
-
-    thread.detach();
+//    std::ofstream o(this->m_tmp_json_file_path);
+//    o << (*m_storage).dump(1, '\t');
 
   }
 
   void ScheduleReceive() {
 
-    this->m_buf.resize(256);
+    this->m_buf.resize(1025);
 
     this->m_responder.async_receive(
         asio::buffer(this->m_buf),
@@ -102,6 +125,10 @@ class ZmqHandler : public asio::io_service {
     return tmp;
   }
 
+  nlohmann::json Set(const nlohmann::json &value) {
+    return Set("", value);
+  }
+
   nlohmann::json Set(const std::string &key, const nlohmann::json &value) {
     nlohmann::json tmp;
     {
@@ -116,12 +143,12 @@ class ZmqHandler : public asio::io_service {
 
 int main(int argc, char **argv) {
 
-  ZmqHandler zmq_handler("/home/slovak/remote-config/config/conf_test.yaml");
+  RemoteConfigSever conf("/home/slovak/remote-config/config/conf_test.yaml");
 
   for (int i = 0; i < 1000; i++) {
     std::this_thread::sleep_for(1s);
 
-    std::cout << "use config: " << zmq_handler.Get("/test/vector/data/0").dump() << std::endl;
+    std::cout << "use config: " << conf("/test/vector/data/0").get<float>() << std::endl;
 
   }
 
