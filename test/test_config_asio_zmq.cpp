66e5710470e6b5257ce9b651e25207d607f50a61
Oleksandr Slovak <slovak194@gmail.com>
2021-01-03 09:40:45 +0100

    cpp server works

--- test/test_config_asio_zmq.cpp
+++ test/test_config_asio_zmq.cpp
@@ -4,6 +4,8 @@
 #include <boost/asio.hpp>
 #include <array>
 #include <vector>
+#include <fstream>
+#include <memory>
 #include <nlohmann/json.hpp>
 
 namespace asio = boost::asio;
@@ -12,14 +14,44 @@ class ZmqHandler {
 
  public:
 
+  std::shared_ptr<nlohmann::json> m_storage;
+
   azmq::rep_socket m_responder;
   std::vector<std::uint8_t> m_buf;
 
-  explicit ZmqHandler(asio::io_service &ios)
+  explicit ZmqHandler(asio::io_service &ios, const std::string &config_path)
   : m_responder(ios) {
     m_responder.bind("tcp://127.0.0.1:5555");
 
     m_buf.reserve(256);
+
+    const std::string tmp_json_file_path = config_path + ".json";
+
+    std::string command = std::string("python3 -c '") +
+        std::string("config_file_yaml_path = \"") + config_path + std::string("\"\n") +
+        "import yaml\n"
+        "import json\n"
+        "with open(config_file_yaml_path, \"r\") as yaml_in, open(\"" + tmp_json_file_path +
+        "\", \"w\") as json_out:\n"
+        "    yaml_object = yaml.safe_load(yaml_in)\n"
+        "    json.dump(yaml_object, json_out)\n"
+        "'";
+
+    std::cout << std::endl << command << std::endl;
+
+    auto res = system(command.c_str());
+
+    if (res) {
+      throw std::runtime_error("Conversion yaml to json has failed with exit code: " + std::to_string(res));
+    }
+
+    std::ifstream i(tmp_json_file_path);
+    m_storage = std::make_shared<nlohmann::json>();
+    *m_storage = nlohmann::json::parse(i);
+
+    std::ofstream o(tmp_json_file_path);
+
+    o << (*m_storage).dump(1, '\t');
 
     ScheduleReceive();
 
@@ -45,10 +77,21 @@ class ZmqHandler {
     nlohmann::json req = nlohmann::json::from_msgpack(m_buf);
     std::cout << req.dump() << std::endl;
 
-    nlohmann::json repl/* = "{'g': 10, 'm_c': 0.39, 'm_p': 0.614, 'h_p': 0.154, 'wheel_radius': 0.035, 'wheel_base': 0.183}"_json*/;
 
-    repl["g"] = 10.0;
-    repl["m_c"] = 0.39;
+    //    nlohmann::json repl/* = "{'g': 10, 'm_c': 0.39, 'm_p': 0.614, 'h_p': 0.154, 'wheel_radius': 0.035, 'wheel_base': 0.183}"_json*/;
+//    repl["g"] = 10.0;
+//    repl["m_c"] = 0.39;
+
+    nlohmann::json repl;
+
+//    auto p = ""_json_pointer;
+
+    if (req["type"].get<std::string>() == "get") {
+      repl = (*m_storage)[nlohmann::json::json_pointer(req["key"].get<std::string>())];
+    } else if (req["type"].get<std::string>() == "set") {
+      (*m_storage)[nlohmann::json::json_pointer(req["key"].get<std::string>())] = req["value"];
+      repl = (*m_storage)[nlohmann::json::json_pointer(req["key"].get<std::string>())];
+    }
 
     const auto msgpack = nlohmann::json::to_msgpack(repl);
 
@@ -67,7 +110,7 @@ int main(int argc, char** argv) {
 
   asio::io_service ios;
 
-  ZmqHandler zmq_handler(ios);
+  ZmqHandler zmq_handler(ios, "/home/slovak/remote-config/config/conf_test.yaml");
 
   ios.run();
 
