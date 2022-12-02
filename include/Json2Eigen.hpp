804760336f6b6274676d1feb2548fcaee488ed84
Oleksandr Slovak <slovak194@gmail.com>
2022-12-02 17:14:35 +0100

    ensure eigen expr evaluated

--- include/Json2Eigen.hpp
+++ include/Json2Eigen.hpp
@@ -29,8 +29,11 @@ struct type {
 namespace Eigen {
 template<typename T>
 void to_json(nlohmann::json &j, const T &matrix) {
-  typename T::Index rows = matrix.rows(), cols = matrix.cols();
-  auto data_begin = const_cast<typename T::Scalar *>(matrix.data());
+
+  auto m = matrix.eval();
+
+  typename T::Index rows = m.rows(), cols = m.cols();
+  auto data_begin = const_cast<typename T::Scalar *>(m.data());
   std::vector<typename T::Scalar> v(data_begin, data_begin + rows * cols);
 
   bool is_row_major = matrix.IsRowMajor;
