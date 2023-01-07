b3bd609780ea158a054ac7d63c6f339c31f406a6
Oleksandr Slovak <slovak194@gmail.com>
2023-01-07 19:52:57 +0100

    + json stuff and doctest

--- include/Json2Eigen.hpp
+++ include/Json2Eigen.hpp
@@ -8,19 +8,21 @@
 #include "Json2Numpy.hpp"
 
 namespace Eigen {
-template<typename T>
-void to_json(nlohmann::json &j, const T &matrix) {
+template<typename Scalar, int Rows, int Cols>
+void to_json(nlohmann::json &j, const Eigen::Matrix<Scalar, Rows, Cols> &matrix) {
+
+  using MatrixType = Eigen::Matrix<Scalar, Rows, Cols>;
 
   auto m = matrix.eval();
 
-  typename T::Index rows = m.rows(), cols = m.cols();
-  auto data_begin = const_cast<typename T::Scalar *>(m.data());
-  std::vector<typename T::Scalar> v(data_begin, data_begin + rows * cols);
+  typename MatrixType::Index rows = m.rows(), cols = m.cols();
+  auto data_begin = const_cast<typename MatrixType::Scalar *>(m.data());
+  std::vector<typename MatrixType::Scalar> v(data_begin, data_begin + rows * cols);
 
   bool is_row_major = matrix.IsRowMajor;
 
   j = {
-      {"dtype", type::dtype<typename T::Scalar>()},
+      {"dtype", type::dtype<typename MatrixType::Scalar>()},
       {"shape", {rows, cols}},
       {"data", v},
       {"order", is_row_major ? "C" : "F"}
--- include/Json2Manif.hpp
+++ include/Json2Manif.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <type_traits>
+#include <string>
 
 #include <Eigen/Core>
 #include <manif/manif.h>
@@ -10,10 +11,82 @@
 
 namespace manif {
 
-template<typename Manifold>
-void to_json(nlohmann::json &j, const Manifold &manifold) {
+template <typename T>
+using BaseType = typename std::remove_cv<typename std::remove_reference<T>::type>::type;
 
-  // TODO
-  j = manifold.coeffs().eval();
+template <typename T> constexpr const char * ElName = "";
+
+template <typename Scalar> inline constexpr const char * ElName<SO2<Scalar>> = "SO2";
+template <typename Scalar> inline constexpr const char * ElName<SO3<Scalar>> = "SO3";
+template <typename Scalar> inline constexpr const char * ElName<SE2<Scalar>> = "SE2";
+template <typename Scalar> inline constexpr const char * ElName<SE3<Scalar>> = "SE3";
+
+template <typename Scalar> inline constexpr const char * ElName<SO2Tangent<Scalar>> = "SO2Tangent";
+template <typename Scalar> inline constexpr const char * ElName<SO3Tangent<Scalar>> = "SO3Tangent";
+template <typename Scalar> inline constexpr const char * ElName<SE2Tangent<Scalar>> = "SE2Tangent";
+template <typename Scalar> inline constexpr const char * ElName<SE3Tangent<Scalar>> = "SE3Tangent";
+
+template <typename Scalar, unsigned int N> inline constexpr const char * ElName<Rn<Scalar, N>> = "Rn";
+template <typename Scalar, unsigned int N> inline constexpr const char * ElName<RnTangent<Scalar, N>> = "RnTangent";
+
+template<typename Scalar, template<typename> class ... Args>
+inline constexpr const char * ElName<Bundle<Scalar, Args...>> = "Bundle";
+
+template <typename T> inline constexpr const char * ElementName = ElName<BaseType<T>>;
+
+template<typename G>
+void to_json(nlohmann::json &j, const G &g) {
+  j["type"] = ElementName<G>;
+//  j["coeffs"] = g.coeffs();
+  j["coeffs"] = std::vector(g.data(), g.data() + g.coeffs().size());
+  assert(!j["type"].template get<std::string>().empty());
 }
+
+template<typename G>
+void from_json(const nlohmann::json &j, G &g) {
+
+  using Scalar = typename G::Scalar;
+  constexpr int size = G::RepSize;
+  for (auto i = 0; i < size; i++) {
+    g.coeffs()(i) = j.at("coeffs")[i];
+  }
 }
+
+template<typename G>
+void from_json(const nlohmann::json &j, Eigen::Map<G> &g) {
+
+  using Scalar = typename G::Scalar;
+  constexpr int size = G::RepSize;
+  for (auto i = 0; i < size; i++) {
+    g.coeffs()(i) = j.at("coeffs")[i];
+  }
+}
+
+template<typename G>
+void to_json(nlohmann::json &j, const Eigen::Map<G> &g) {
+    j = G(g);
+}
+
+template<typename Scalar, template<typename> class ... Args>
+void to_json(nlohmann::json &j, const Bundle<Scalar, Args...> &bundle) {
+  j["type"] = "Bundle";
+  j["elements"] = nlohmann::json::array();
+  j["coeffs"] = std::vector(bundle.data(), bundle.data() + bundle.coeffs().size());;
+
+  using ThisBundle = Bundle<Scalar, Args...>;
+
+  auto fill = [](nlohmann::json &j, std::size_t i, auto el) {
+    j["elements"][i] = el;
+  };
+
+  auto lam = [&]<std::size_t... I>(std::index_sequence<I...>) {
+    (fill(j, I, bundle.template element<I>()), ...);
+  };
+
+  lam(std::make_index_sequence<internal::traits<ThisBundle>::BundleSize>{});
+
+  assert(!j["type"].template get<std::string>().empty());
+}
+
+}
+
